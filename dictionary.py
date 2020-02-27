589732674f7a36c1e762fa043eec84476e99ecae
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-02-27 23:22:16 +0530

    Dictionary

---
+++ dictionary.py
@@ -0,0 +1,7 @@
+>>> dictionary={'name':'charlie','id':100,'dept':'it'}  
+>>> dictionary  
+{'dept': 'it', 'name': 'charlie', 'id': 100}  
+>>> dictionary.keys()  
+['dept', 'name', 'id']  
+>>> dictionary.values()  
+['it', 'charlie', 100] 
