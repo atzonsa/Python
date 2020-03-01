9d518277d14405bb8e016956d18e532026f14396
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-03-01 22:21:22 +0530

    dictionary

--- dictionary.py
+++ dictionary.py
@@ -5,3 +5,11 @@
 ['dept', 'name', 'id']  
 >>> dictionary.values()  
 ['it', 'charlie', 100] 
+
+
+d = {1:'Jimmy', 2:'Alex', 3:'john', 4:'mike'};   
+print("1st name is "+d[1]);  
+print("2nd name is "+ d[4]);  
+print (d);  
+print (d.keys());  
+print (d.values());  
