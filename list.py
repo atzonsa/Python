69b172ce975d1784812d58dec7a96389e013e251
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-03-03 21:12:54 +0530

    list

--- list.py
+++ list.py
@@ -4,3 +4,17 @@ print (l[0:2]);
 print (l);  
 print (l + l);  
 print (l * 3);   
+
+
+
+>>> list=['aman',678,20.4,'saurav']  
+>>> list1=[456,'rahul']  
+>>> list  
+['aman', 678, 20.4, 'saurav']  
+>>> list[1:3]  
+[678, 20.4]  
+>>> list+list1  
+['aman', 678, 20.4, 'saurav', 456, 'rahul']  
+>>> list1*2  
+[456, 'rahul', 456, 'rahul']  
+>>>  
