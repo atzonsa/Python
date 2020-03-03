dc3b6901b4ef7b7668568669d13ce0274b7562a8
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-03-03 21:15:54 +0530

    print in angle

---
+++ printinangle.py
@@ -0,0 +1,6 @@
+i,j = 0,0;
+whatoprint= str(input("What to print"))
+howmanytimes = int(input("Please enter how many times do you want to print"))
+
+for i in range(1,howmanytimes+1):
+        print(whatoprint*i)
