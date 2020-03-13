c45dc1df73027a6d29893bd34f07448db3deb2bf
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-03-13 22:03:26 +0530

    for loop else

---
+++ forloopelse.py
@@ -0,0 +1,11 @@
+for i in range(0,5):  
+    print(i)  
+else:print("for loop completely exhausted, since there is no break.");  
+
+
+for i in range(0,5):  
+    print(i)  
+    break;  
+else:print("for loop is exhausted");  
+    print("The loop is broken due to break statement...came out of loop")
+
