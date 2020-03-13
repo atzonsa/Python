740b10709a4b5f04435e99e6087382533fa16f48
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-03-13 22:03:39 +0530

    while loop

---
+++ whileloop.py
@@ -0,0 +1,26 @@
+i=1
+while i<=10:
+    print(i*1);
+    i+=1;
+
+i = int(input("Enter the number"));
+ordinal = 1
+while ordinal <= 10:
+    print(i*ordinal);
+    ordinal+=1;
+
+count = 1
+while 1:
+    print("found")
+    count+=1;
+    if (count == 10):
+        break;
+
+var = 1  
+    count = 1
+    while var != 2:  
+        i = int(input("Enter the number?"))  
+        print ("Entered value is %d"%(i))  
+        count+=1
+        if count == 5:
+            break;
